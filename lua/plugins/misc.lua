d9778b64fa09233eb53c56ceaad5816155bf85ca
Daniel Dostal <daniel.dostal.sk@gmail.com>
2025-01-02 21:51:40 +0100

    Add typescript commenting plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -29,6 +29,23 @@ return {
 		opts = { signs = false },
 	},
 	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		config = true,
+		opts = {
+			enable_autocmd = false,
+		},
+	},
+	{
+		"b3nj5m1n/kommentary",
+		config = function()
+			require("kommentary.config").configure_language("default", {
+				single_line_comment_string = "auto",
+				multi_line_comment_strings = "auto",
+				hook_function = function() require("ts_context_commentstring").update_commentstring() end,
+			})
+		end,
+	},
+	{
 		-- Highlight hex colors
 		"norcalli/nvim-colorizer.lua",
 		config = function() require("colorizer").setup() end,
