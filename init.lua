da958563d6a88189520206a50b3c23ec3f8d5fed
Daniel Dostál <daniel.dostal.sk@gmail.com>
2025-02-18 18:28:11 +0100

    Add git blame

--- init.lua
+++ init.lua
@@ -3,39 +3,39 @@ require "core.keymaps"
 
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 local themes = require "plugins.colorthemes"
 
 require("lazy").setup {
-  themes.plugins,
-  require "plugins.alpha",
-  require "plugins.autocompletion",
-  --  require 'plugins.bufferline',
-  require "plugins.copilot",
-  require "plugins.gitsigns",
-  require "plugins.harpoon",
-  require "plugins.indent-blankline",
-  require "plugins.lazygit",
-  require "plugins.lsp",
-  require "plugins.lualine",
-  require "plugins.misc",
-  require "plugins.none-ls",
-  require "plugins.oil",
-  require "plugins.telescope",
-  require "plugins.treesitter",
-  require "plugins.typescript-tools",
-  require "plugins.undotree",
-  require "plugins.mini-ai",
+	themes.plugins,
+	require "plugins.alpha",
+	require "plugins.autocompletion",
+	--  require 'plugins.bufferline',
+	require "plugins.copilot",
+	require "plugins.gitsigns",
+	require "plugins.harpoon",
+	require "plugins.indent-blankline",
+	require "plugins.lazygit",
+	require "plugins.lsp",
+	require "plugins.lualine",
+	require "plugins.misc",
+	require "plugins.none-ls",
+	require "plugins.oil",
+	require "plugins.telescope",
+	require "plugins.treesitter",
+	require "plugins.typescript-tools",
+	require "plugins.undotree",
+	require "plugins.mini-ai",
 
-  -- TODO: tmux
-  -- TODO: overseer
+	-- TODO: tmux
+	-- TODO: overseer
 }
 
 themes.setup()
